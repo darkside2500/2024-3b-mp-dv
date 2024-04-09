f243f64ac8e7cfad4e2b3f2563f58edc46705945
marcao <106687674+darkside2500@users.noreply.github.com>
2024-04-09 07:56:16 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 2024-3B-MeusProjetosDeVida
+# 2024-3b-meus-projetos-de-vida
--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const botoes = document.querySelectorAll('.botao');
-const textos = document.querySelectorAll('.aba-conteudo');
+const textos = document.querySelectorAll('.aba-conteudo')
 
-const contadores = document.querySelector('contador');
+const contadores = document.querySelectorAll('.contador')
 
-const tempObjetivo01=new Date('2025-02-05T00:00:00');
-const tempObjetivo02=new Date('2024-12-31T00:00:00');
-const tempObjetivo03=new Date('2025-02-08T00:00:00');
-const tempObjetivo04=new Date('2025-02-05T00:00:00');
+const tempoObjetivo01 = new Date("2025-02-05T00:00:00")
+const tempoObjetivo02 = new Date("2025-01-25T00:00:00")
+const tempoObjetivo03 = new Date("2025-01-01T00:00:00")
+const tempoObjetivo04 = new Date("2026-01-10T00:00:00")
 
-const tempos = [tempObjetivo01,tempObjetivo02,tempObjetivo03,tempObjetivo04]
+const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
-for (let i = 0; i<tempos.length; i ++){
+for (let i= 0; i<tempos.length; i++){
     contadores[i].textContent = calculaTempo(tempos[i])
 }
+
 
 for(let i= 0; i<botoes.length; i++){
     botoes[i].onclick = function (){
@@ -20,26 +21,27 @@ for(let i= 0; i<botoes.length; i++){
         for(j=0; j<botoes.length; j++){
             botoes[j].classList.remove('ativo')
             textos[j].classList.remove('ativo')
-
         }
 
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
     } 
 }
-function calculatempo (tempObjetivo){
-    let tempoAtual = new date ()
-    let tempofinal = tempObjetivo - tempoAtual
 
-    let segundos = Math.floor(tempofinal/1000);
-    let minutos = Math.floor(minutos/60);
-    let horas =Math.floor(horas/60);
-    let dias =Math.floor(dias/24);
+function calculaTempo (tempoObjetivo) {
 
-segundos %= 60;
-minutos%=60;
-horas%=24;
+    let tempoAtual = new Date()
+    let tempoFinal = tempoObjetivo - tempoAtual
 
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
 
-return dias + "dias"+ horas + " horas " + minutos +"minutos " + segundos + "segundos"
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
+
 }
